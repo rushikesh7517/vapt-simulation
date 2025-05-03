6c40c567b3576647c26a22b16ca6a38f094b5485
Rushikesh <rushikeshgilbile@gmail.com>
2025-05-03 11:42:16 +0530

    the reamining  feature is added

--- project/theharvester/views.py
+++ project/theharvester/views.py
@@ -1,15 +1,13 @@
 from django.shortcuts import render
-import subprocess, os
-from datetime import datetime
-from django.conf import settings
+import subprocess
 from django.contrib.auth.decorators import login_required
 
 @login_required(login_url='login')
 def harvester_scan_view(request):
     if request.method == 'POST':
-        target = request.POST['target']
-        source = request.POST['source']
-        limit = request.POST['limit']
+        target = request.POST.get('target')
+        source = request.POST.get('source')
+        limit = request.POST.get('limit')
 
         # theHarvester command
         cmd = [
@@ -24,25 +22,11 @@ def harvester_scan_view(request):
         except subprocess.CalledProcessError as e:
             result = e.output
 
-        # Save Result to File
-        harvest_dir = os.path.join(settings.BASE_DIR, 'harvest_results')
-        os.makedirs(harvest_dir, exist_ok=True)
-        timestamp = datetime.now().strftime('%Y%m%d_%H%M%S')
-        safe_target = target.replace('.', '_')
-        filename = f"harvest_{safe_target}_{timestamp}.txt"
-        filepath = os.path.join(harvest_dir, filename)
-
-        with open(filepath, 'w') as f:
-            f.write(f"Scan Report for: {target} using {source}\n\n")
-            f.write(result)
-
         context = {
             'result': result,
             'target': target,
-            'source': source,
-            'file_path': filename
+            'source': source
         }
         return render(request, 'theharvester/result.html', context)
 
     return render(request, 'theharvester/harvester.html')
-
